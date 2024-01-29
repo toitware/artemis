db5bc7e3ec7e5605681e3bb02ffd672f27875c7e
Florian Loitsch <florian@toit.io>
2024-01-29 21:07:10 +0400

    Add installer.

---
+++ tools/windows_installer/build.bat
@@ -0,0 +1,10 @@
+set VERSION=%1
+set BIN_PATH=%2
+set BUILD_DIRECTORY=%3
+
+set INSTALLER_NAME="%cd%\tools\windows_installer\artemis_installer.exe"
+
+set INNO=ISCC.exe
+
+%INNO% /Qp /dMyAppVersion=%VERSION% /dMyAppExeName="%BIN_PATH%" "%cd%/tools/windows_installer/installer.iss"
+move %INSTALLER_NAME% %BUILD_DIRECTORY%
